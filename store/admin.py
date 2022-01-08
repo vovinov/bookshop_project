51330cbd896329cfab34b6b86116bdf5a9e7e856
vovinov <v.v.ovinov@yandex.ru>
2022-01-08 23:04:36 +0300

    Add new config to admin panel, reset db

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'in_stock']
+    list_editable = ['price', 'in_stock']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
--- store/models.py
+++ store/models.py
@@ -3,17 +3,28 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=255, editable=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    description = models.TextField()
-    photo = models.ImageField()
-    price = models.IntegerField()
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='media/')
+    price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.IntegerField()
-    available = models.BooleanField()
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, editable=True)
+
+    def __str__(self):
+        return self.name
+
+
